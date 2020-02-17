2cdea589ac05a7932ab7eb2dafdb02171b2a102f
unknown <kping22@gmail.com>
2020-02-17 19:16:20 +0900

    Add arduino_blue_to_smart/

---
+++ arduino_blue_to_smart/arduino_blue_to_smart.ino
@@ -0,0 +1,64 @@
+#include <SoftwareSerial.h>
+// SoftwareSerial(RX, TX)
+SoftwareSerial BTSerial(2, 3);
+
+int dustPin = A0;
+float dustVal = 0;
+float dustDensity = 0;
+float dustDensityug = 0;
+float voMeasured = 0;
+float calcVoltage = 0;
+float sumVoltage =0;
+byte *dustbyte = (byte *) &dustDensityug;
+
+int index = 0;
+int ledPower = 4;
+int delayTime = 280;
+int delayTime2 = 40;
+float offTime = 9680;
+
+void setup(){
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+  pinMode(ledPower,OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(ledPower, LOW); // power on the LED
+  delayMicroseconds(delayTime);
+
+  dustVal=analogRead(dustPin);
+  calcVoltage = dustVal * (5.0 / 1024); 
+  
+  delayMicroseconds(delayTime2);
+
+  digitalWrite(ledPower, HIGH); // turn the LED off
+  delayMicroseconds(offTime);
+  
+  delay(1000);
+  if(index == 10) {
+    dustDensityug = dustDensityug/10;
+    sumVoltage = sumVoltage/10;
+    BTSerial.write(dustbyte, 4);
+    BTSerial.write(".");
+    Serial.print("Dust Density [ug/m3]: ");
+    Serial.println(dustDensityug);
+    Serial.print("votage : ");
+    Serial.println(sumVoltage);
+    dustDensityug = 0;
+    sumVoltage = 0;
+    index = 0;
+  }
+  else {
+    sumVoltage = sumVoltage + calcVoltage;
+    dustDensity = (0.17 * calcVoltage - 0.11)*1000;
+    if(dustDensity < 0 || dustDensity > 200 ) {
+      dustDensityug = dustDensityug + 0;
+    }
+    else {
+      dustDensityug = dustDensityug + dustDensity;
+    }
+    index++; 
+  }
+}
